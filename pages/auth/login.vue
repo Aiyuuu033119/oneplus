4bb159c65f2d2cc26dfdb7b006b1838d9d4eadd0
marinellareisarmiento <marinellasarmiento@idnplay.com>
2022-10-01 14:21:56 +0800

    add login

---
+++ pages/auth/login.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container w-50">
+        <Login/>
+    </div>
+</template>
+<script>
+    import Login from '~/components/Login.vue';
+    export default {
+    
+      name: 'login',
+    
+      layout: 'pages',
+    
+      components: {
+        Login
+      },
+    
+      data () {
+        return {
+            // news : null,
+        }
+      },
+    //   async fetch() {
+    //       this.news = await fetch(process.env.apiUrl+'/mobileapi/news_all').then((res) => res.json());
+    //       console.log(this.news);
+    //   }
+    }
+    </script>
