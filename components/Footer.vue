cd92c44d2fb8f43f9fd0a10e2b0ef5cdf0bffac1
ernestbaquir <ernestbaquir@idnplay.com>
2022-10-01 13:31:51 +0800

    Header, Footer and Side Nav. Header, Footer and Side Nav responsiveness.

--- components/Footer.vue
+++ components/Footer.vue
@@ -9,7 +9,7 @@
             <nuxt-link to="" class="text-dark">
               <div class="card border-0 rounded shadow p-4">
                 <div class="d-flex align-items-center flex-column gap-2">
-                  <Icon icon="logos:whatsapp" height="52px"/>
+                  <Icon icon="logos:whatsapp" height="52px" />
                   <div>Whatsapp</div>
                 </div>
               </div>
@@ -19,7 +19,11 @@
             <nuxt-link to="" class="text-dark">
               <div class="card border-0 rounded shadow p-4">
                 <div class="d-flex align-items-center flex-column gap-2">
-                  <Icon icon="dashicons:email" height="52px" class="text-danger"/>
+                  <Icon
+                    icon="dashicons:email"
+                    height="52px"
+                    class="text-danger"
+                  />
                   <div>Email</div>
                 </div>
               </div>
@@ -29,7 +33,11 @@
             <nuxt-link to="" class="text-dark">
               <div class="card border-0 rounded shadow p-4">
                 <div class="d-flex align-items-center flex-column gap-2">
-                  <Icon icon="fluent:chat-help-24-filled" height="52px" class="text-danger"/>
+                  <Icon
+                    icon="fluent:chat-help-24-filled"
+                    height="52px"
+                    class="text-danger"
+                  />
                   <div>FAQ</div>
                 </div>
               </div>
@@ -39,7 +47,11 @@
             <nuxt-link to="" class="text-dark">
               <div class="card border-0 rounded shadow p-4">
                 <div class="d-flex align-items-center flex-column gap-2">
-                  <Icon icon="si-glyph:customer-support" height="52px" class="text-danger"/>
+                  <Icon
+                    icon="si-glyph:customer-support"
+                    height="52px"
+                    class="text-danger"
+                  />
                   <div>Hotline</div>
                 </div>
               </div>
@@ -48,19 +60,86 @@
         </div>
       </div>
     </section>
-    <section class="bg-light">
+    <section class="bg-light border-bottom pb-2">
       <div class="container footer-sub">
-        <div class="mb-4"><img :src="logo" height="25px"></div>
+        <div class="mb-4"><img :src="logo" height="25px" /></div>
         <div class="row">
-          <div class="col-12 col-lg-4 mb-3"><small>{{alamat}}</small></div>
+          <div class="col-12 col-lg-4 mb-4">
+            <small>{{ alamat }}</small>
+            <p class="m-0"><strong>PT Maju Selaras Komputer</strong></p>
+            <p class="m-0">
+              Ruko Citra Bisnis Park, Jalan Peta Barat Blok G No 23
+            </p>
+            <p class="m-0">Kalideres - Jakarta Barat</p>
+          </div>
+          <div class="col-12 col-lg-8 footer-menu mt-4">
+            <div class="d-flex align-items-center gap-3 justify-content-end">
+              <nuxt-link to="" class="text-dark">Syarat & Ketentuan</nuxt-link>
+              <nuxt-link to="" class="text-dark">Hubungi Kami</nuxt-link>
+            </div>
+
+            <div class="mt-3 d-flex align-items-center gap-3 justify-content-end">
+              <nuxt-link to="" class="text-dark">
+                <div class="social facebook border-0 rounded-circle p-2">
+                  <div class="d-flex align-items-center flex-column gap-2">
+                    <Icon icon="brandico:facebook" height="24px" class="text-white"/>
+                  </div>
+                </div>
+              </nuxt-link>
+              <nuxt-link to="" class="text-dark">
+                <div class="social email border-0 rounded-circle p-2">
+                  <div class="d-flex align-items-center flex-column gap-2">
+                    <Icon
+                      icon="bxs:envelope"
+                      height="24px"
+                      class="text-white"
+                    />
+                  </div>
+                </div>
+              </nuxt-link>
+              <nuxt-link to="" class="text-dark">
+                <div class="social twitter border-0 rounded-circle p-2">
+                  <div class="d-flex align-items-center flex-column gap-2">
+                    <Icon
+                      icon="akar-icons:twitter-fill"
+                      height="24px"
+                      class="text-white"
+                    />
+                  </div>
+                </div>
+              </nuxt-link>
+              <nuxt-link to="" class="text-dark">
+                <div class="social instagram border-0 rounded-circle p-2">
+                  <div class="d-flex align-items-center flex-column gap-2">
+                    <Icon
+                      icon="akar-icons:instagram-fill"
+                      height="24px"
+                      class="text-white"
+                    />
+                  </div>
+                </div>
+              </nuxt-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="bg-light pt-3 pb-5">
+      <div class="container footer-sub">
+        <div class="row">
+          <div class="col-12 col-lg-4 mb-3">
+            <small>© 2020 - 2022 VOGAME. All Rights Reserved</small>
+          </div>
           <div class="col-12 col-lg-8 footer-menu">
-            <ul class="">
-              <li><nuxt-link to="/page/contact-us" class="btn-link text-dark">Pemasaran dan Kemitraan</nuxt-link></li>
-              <li><nuxt-link to="/page/terms-and-conditions" class="btn-link text-dark">Syarat & Ketentuan</nuxt-link></li>
-              <li><nuxt-link to="/page/privacy-policy" class="btn-link text-dark">Kebijakan & Privasi</nuxt-link></li>
-              <li><nuxt-link to="/news" class="btn-link text-dark">Berita & Acara</nuxt-link></li>
-            </ul>
-            <div class="text-secondary"><small>&copy; Hak Cipta {{title}}</small></div>
+            <small
+              >Kembali keatas
+              <Icon
+                icon="akar-icons:arrow-up"
+                height="16px"
+                class="text-black"
+              />
+            </small>
           </div>
         </div>
       </div>
@@ -69,41 +148,61 @@
 </template>
 
 <script>
-import { Icon } from '@iconify/vue2';
+import { Icon } from "@iconify/vue2";
 export default {
-
-  name: 'Footer',
+  name: "Footer",
 
   components: {
-    Icon
+    Icon,
   },
 
-  data () {
+  data() {
     return {
-        title : 'Isigame',
-        logo : 'img/logo.svg',
-        alamat : '',
-        notelp : '',
-        whatsapp : ''
-    }
+      title: "Isigame",
+      logo: "img/logo.svg",
+      alamat: "",
+      notelp: "",
+      whatsapp: "",
+    };
   },
-  created(){
-      var vuedis = this;
-      this.$axios.get('/mobileapi/s7xs6oast').then((result) => {
-            vuedis.title = result.data.nama;
-            vuedis.logo = result.data.logo;
-            vuedis.notelp = result.data.telp;
-            vuedis.alamat = result.data.alamat;
-            vuedis.whatsapp = result.data.wa;
-      });
-  }
-}
+  created() {
+    var vuedis = this;
+    this.$axios.get("/mobileapi/s7xs6oast").then((result) => {
+      vuedis.title = result.data.nama;
+      vuedis.logo = result.data.logo;
+      vuedis.notelp = result.data.telp;
+      vuedis.alamat = result.data.alamat;
+      vuedis.whatsapp = result.data.wa;
+    });
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-  .card{
-    &:hover{
-      color: #ff5a5e;
-    }
+.card {
+  &:hover {
+    color: #ff5a5e;
   }
+}
+.social {
+  color: white;
+  height: 42px;
+  width: 42px;
+}
+
+.social.facebook {
+  background: #2c4cd5;
+}
+.social.email {
+  background: #db4437;
+}
+.social.twitter {
+  background: #1dcaff;
+}
+.social.instagram {
+  background: #dd2a7b;
+}
+.social:hover {
+  background: #ff5a5e;
+}
 </style>
