59eabc6f7ac137a3942a180f654ec1d2247410ab
IanDestura <aiyuuu033119@gmail.com>
2022-10-01 17:59:55 +0800

    add contact

---
+++ pages/contact-us.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container mt-5">
+    <div class="m-b-30 text-center">
+      <h1><b>Contact Us</b></h1>
+    </div>
+    <ol>
+      <p>
+        <strong>PT Maju Selaras Komputer</strong><br />Ruko Citra Bisnis Park,
+        Jalan Peta Barat Blok G No 23 Kalideres - Jakarta Barat
+      </p>
+      <p>
+        <span
+          style="
+            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+              Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue',
+              sans-serif;
+          "
+          ><strong>Office Hour</strong> <br />08:30 WIB s/d 17:30
+          WIB&nbsp;</span
+        >
+      </p>
+      <p>
+        <strong>Hubungi Kami</strong><br />Whatsapp : 0813-8866-7422<br />E-Mail
+        :&nbsp;<span
+          style="
+            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+              Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue',
+              sans-serif;
+          "
+          >cs@vogame.co.id</span
+        >
+      </p>
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+    </ol>
+    <div class="m-b-80"></div>
+  </div>
+</template>
+<script>
+  export default {
+    name: "index",
+    layout: "default",
+  };
+  </script>
+  
