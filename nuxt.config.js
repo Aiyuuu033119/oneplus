a69fbf338ff898bf469ada00043290b0308b6913
IanDestura <aiyuuu033119@gmail.com>
2022-10-03 15:14:00 +0800

    add images

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '@/assets/css/duniasegame.scss',
+    '@/assets/css/gamespedia.scss',
     '@/assets/css/main.scss',
   ],
 
